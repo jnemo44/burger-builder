0afd56ffd8a9bf8f859563d52ec83bdf07f2c7b3
jnemo44 <64214120+jnemo44@users.noreply.github.com>
2020-09-23 20:36:23 -0500

    Ended up merging with instructor code as I had an issue rendering a burger

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,14 @@
 import React from 'react';
+
 import Aux from '../../hoc/Auxilary';
-import classes from './Layout.css'
+import classes from './Layout.css';
 
 const layout = ( props ) => (
     <Aux>
-    <div>Toolbar, SideDrawer, Backdrop</div>
-    <main className={classes.Content}>
-        {props.children}
-    </main>
+        <div>Toolbar, SideDrawer, Backdrop</div>
+        <main className={classes.Content}>
+            {props.children}
+        </main>
     </Aux>
 );
 
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 
-import Aux from '../../hoc/Auxilary'
+import Aux from '../../hoc/Auxilary';
+import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
-    render() {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {...}
+    // }
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 1,
+            meat: 2
+        }
+    }
+
+    render () {
         return (
             <Aux>
-               <div>Burger</div> 
-               <div>Build Controls</div>
+                <Burger ingredients={this.state.ingredients} />
+                <div>Build Controls</div>
             </Aux>
         );
     }
