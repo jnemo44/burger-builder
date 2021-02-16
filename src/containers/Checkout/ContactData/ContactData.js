3a5703e7944ac63498b0ae8bc34e593f89a61acc
jnemo44 <64214120+jnemo44@users.noreply.github.com>
2021-02-16 07:33:01 -0600

    Began adding input component to build a form

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -4,6 +4,7 @@ import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.css';
 import axios from '../../../axios-orders';
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
     state = {
@@ -49,10 +50,10 @@ class ContactData extends Component {
     render () {
         let form = (
             <form>
-                <input className={classes.Input} type='text' name='name' placeholder='Your Name'/>
-                <input className={classes.Input} type='text' name='email' placeholder='Your Email'/>
-                <input className={classes.Input} type='text' name='street' placeholder='Your Street'/>
-                <input className={classes.Input} type='text' name='zip' placeholder='Your zip code'/>
+                <Input inputtype='input' type='text' name='name' placeholder='Your Name'/>
+                <Input inputtype='input' type='text' name='email' placeholder='Your Email'/>
+                <Input inputtype='input' type='text' name='street' placeholder='Your Street'/>
+                <Input inputtype='input' type='text' name='zip' placeholder='Your zip code'/>
                 <Button btnType="Success" clicked={this.orderHandler}>SUBMIT ORDER</Button>
             </form>);
         if (this.state.loading) {
