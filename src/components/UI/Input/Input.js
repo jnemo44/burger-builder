99f73cf8431d7288176c215401789563bbb58624
jnemo44 <64214120+jnemo44@users.noreply.github.com>
2021-03-02 21:03:35 -0600

    Adding more user friendly validation

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -4,11 +4,17 @@ import classes from './Input.css';
 
 const input = (props) => {
     let inputElement = null;
+    const inputClasses = [classes.InputElement];
+
+    if (props.invalid && props.shouldValidate && props.touched) {
+        inputClasses.push(classes.Invalid);
+        inputClasses.push(classes.InvalidBold);
+    }
 
     switch (props.elementType) {
         case ('input'):
             inputElement = <input 
-                className={classes.InputElement}
+                className={inputClasses.join(' ')}
                 {...props.elementConfig}
                 value={props.value}
                 onChange={props.changed}/>
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -19,7 +19,8 @@ class ContactData extends Component {
                 validation: {
                     required: true
                 },
-                valid : false
+                valid: false,
+                touched: false 
             },
             email: {
                 elementType: 'input',
@@ -31,7 +32,8 @@ class ContactData extends Component {
                 validation: {
                     required: true
                 },
-                valid : false
+                valid : false,
+                touched: false
             },
             street: {
                 elementType: 'input',
@@ -43,7 +45,8 @@ class ContactData extends Component {
                 validation: {
                     required: true
                 },
-                valid : false
+                valid : false,
+                touched: false
             },
             zipCode: {
                 elementType: 'input',
@@ -57,7 +60,8 @@ class ContactData extends Component {
                     minLength: 5,
                     maxLength: 5,
                 },
-                valid : false
+                valid : false,
+                touched: false
             },
             deliveryMethod: {
                 elementType: 'select',
@@ -67,9 +71,13 @@ class ContactData extends Component {
                         {value: 'cheapest', displayValue: 'Cheapest'}
                     ]
                 },
+                validation: {
+                    required: false
+                },
                 value: 'fastest'
             },
         },
+        formIsValid: false,
         loading: false
     }
 
@@ -123,6 +131,7 @@ class ContactData extends Component {
         };
         updatedFormElement.value = event.target.value;
         updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
+        updatedFormElement.touched = true;
         updatedOrderForm[inputIdentifier] = updatedFormElement;
         console.log(updatedFormElement);
         this.setState({orderForm: updatedOrderForm});
@@ -143,6 +152,9 @@ class ContactData extends Component {
                         elementType={formElement.config.elementType}
                         elementConfig={formElement.config.elementConfig}
                         value={formElement.config.value}
+                        invalid={!formElement.config.valid}
+                        shouldValidate={formElement.config.validation}
+                        touched={formElement.config.touched}
                         changed={(event) => this.inputChangedHandler(event, formElement.id)}/>
                 ))}         
                 <Button btnType="Success">SUBMIT ORDER</Button>
