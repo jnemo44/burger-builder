d603c49d84c937678088ecaed81ba482b31e910b
jnemo44 <64214120+jnemo44@users.noreply.github.com>
2021-03-08 20:56:28 -0600

    Finished adding overall form validation and removed a bug in the select element

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -74,6 +74,7 @@ class ContactData extends Component {
                 validation: {
                     required: false
                 },
+                valid : true,
                 value: 'fastest'
             },
         },
@@ -133,8 +134,13 @@ class ContactData extends Component {
         updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
         updatedFormElement.touched = true;
         updatedOrderForm[inputIdentifier] = updatedFormElement;
-        console.log(updatedFormElement);
-        this.setState({orderForm: updatedOrderForm});
+        
+        let formIsValid = true;
+        for (let inputIdentifier in updatedOrderForm) {
+            formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+        }
+        console.log(formIsValid);
+        this.setState({orderForm: updatedOrderForm, formIsValid: formIsValid});
     }
     render () {
         const formElementsArray = [];
@@ -157,7 +163,7 @@ class ContactData extends Component {
                         touched={formElement.config.touched}
                         changed={(event) => this.inputChangedHandler(event, formElement.id)}/>
                 ))}         
-                <Button btnType="Success">SUBMIT ORDER</Button>
+                <Button btnType="Success" disabled={!this.state.formIsValid}>SUBMIT ORDER</Button>
             </form>);
         if (this.state.loading) {
             form = <Spinner/>;
